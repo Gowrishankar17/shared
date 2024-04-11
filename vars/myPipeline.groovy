e23339aceae05638a7655f9ffcd0a5eb5f952910
Gowrishankar17 <158024736+Gowrishankar17@users.noreply.github.com>
2024-04-11 15:55:08 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -2,17 +2,17 @@ def call() {
     pipeline {
         agent any
         stages {
-            // stage('Git Checkout') {
-            //     steps {
-            //         script {
-            //             git branch: 'node-dev', url: 'https://github.com/Gowrishankar17/multi-branch.git' //project-repo
-            //         }
-            //     }
-            // }
+            stage('Git Checkout') {
+                steps {
+                    script {
+                        git branch: 'master', url: 'https://github.com/Gowrishankar17/multi-branch.git' //project-repo
+                    }
+                }
+            }
             stage ('Build') {
                 steps {
                     script {
-                        sh 'npm install'
+                        sh 'mvn install'
                     }
                 }
             }
